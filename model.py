0206d204507f68e8e5377ef98be8b3740d5f26a8
Dayanna Espinoza <espinoza@mit.edu>
2017-01-11 20:49:44 -0500

    board is a dictionary, positions are numbers from 1-9. next turn still shows up when there the game ends

--- model.py
+++ model.py
@@ -8,29 +8,23 @@ class Tictactoe:
 		self.o = other_player
 		self.turn = self.x
 		#empyt board
-		self.board = [["-", "-", "-"],["-", "-", "-"], ["-", "-", "-"]]
+		self.board = {1:"-", 2:"-", 3:"-", 4:"-", 5:"-", 6:"-", 7:"-", 8:"-", 9:"-"}
+		# self.board = {1:"a", 2:"b", 3:"c", 4:"d", 5:"e", 6:"f", 7:"g", 8:"h", 9:"i"}
 		
 
 
 	def getBoard(self):
 		out = ""
-		for r in range(len(self.board)):
-			out += "| "
-			for c in self.board[r]:
-				out += c + " | "
-			if r != 2:
-				out += "\n" + "|---+---+---|" + "\n"
+		out += "| " + self.board[1] + " | " + self.board[2] + " | " + self.board[3] + " |"
+		out += "\n" + "|---+---+---|" + "\n"
+		out += "| " + self.board[4] + " | " + self.board[5] + " | " + self.board[6] + " |"
+		out += "\n" + "|---+---+---|" + "\n"
+		out += "| " + self.board[7] + " | " + self.board[8] + " | " + self.board[9] + " |"
 		if self.turn == self.x: 
 			return out + "\nturn: " + self.x
 		else: 
 			return out + "\nturn: " + self.o
-		# for i in self.board
-		# 	out += "| " + str(i) + " "
-		# 	if i==2 or i==5 :
-		# 		out += "| \n" + "|---+---+---|" + "\n"
-		# 	if i==8:
-		# 		out += "|"
-		# return out
+
 
 
 	def updateTurn(self):
@@ -39,24 +33,28 @@ class Tictactoe:
 		else:
 			self.turn = self.x
 
-	def addMark(self, player, move):
+
+	def addMark(self, player):
 		if player == self.x:
 			return "x"
 		else:
 			return "o"
 
-	#move is a tuple (row,column)
+	#move is a position: 1,2,3,..,9
 	def makeMove(self, player, move):
 		#check if it is player's turn
 		if player != self.turn:
 			return "This isn't your turn"
 		#check if move if valid
-		if move[0] < 0 or move[0]>2 or move[1] < 0 or move[1]>2:
+		if move not in self.board:
 			return "Invalid move"
 		
 		#add that move to the board and change turns
-		if self.board[move[0]][move[1]] == "-":
-			self.board[move[0]][move[1]] = self.addMark(player)
+		if self.board[move] == "-":
+			self.board[move] = self.addMark(player)
+			if self.isGameOver():
+				return self.getBoard() + "\n"+self.isGameOver()
+			# self.isGameOver()
 			self.updateTurn()
 			return self.getBoard()
 
@@ -67,22 +65,17 @@ class Tictactoe:
 	#self.board = [["-", "-", "-"],["-", "-", "-"], ["-", "-", "-"]]
 	def isGameOver(self):
 		#check rows:
-		for row in self.board:
-			x_counter = 0
-			o_counter = 0
-			for mark in row:
-				if mark == 'x':
-					x_counter += 1
-				elif mark == 'o':
-					o_counter +=1
-			if x_counter == 3:
-				return self.x
-			elif o_counter == 3:
-				return self.o
-
-		#check colums:
-
-		#check diagonals:
+		if ((self.board[1] == self.board[2] == self.board[3] != "-") or 
+			(self.board[4] == self.board[5] == self.board[6] != "-") or 
+			(self.board[7] == self.board[8] == self.board[9] != "-") or 
+			(self.board[1] == self.board[4] == self.board[7] != "-") or 
+			(self.board[2] == self.board[5] == self.board[8] != "-") or 
+			(self.board[3] == self.board[6] == self.board[9] != "-") or 
+			(self.board[1] == self.board[5] == self.board[9] != "-") or 
+			(self.board[3] == self.board[5] == self.board[7] != "-")):
+			return "winner is "  + self.turn 
+		else: 
+			return None
 
 
 
@@ -91,8 +84,15 @@ class Tictactoe:
 
 
 game = Tictactoe("me", "you")
-game.makeMove(game.x, (0,0))
-print game.makeMove(game.o, (0,1))
+print "me " + game.makeMove(game.x, 1) + "\n"
+print "you " + game.makeMove(game.o, 2) +"\n"
+print "me " + game.makeMove(game.x, 4)+"\n"
+# print game.isGameOver()
+print "you " +game.makeMove(game.o, 3) +"\n"
+print "me " + game.makeMove(game.x, 7)+"\n"
+# print game.isGameOver()
+# print game.makeMove(game.o, 5)
+# print game.isGameOver()
 # print game.getBoard()
 
 
